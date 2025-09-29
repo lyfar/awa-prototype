1b745345bf2d7b2999487a9066fbbb252f543c8e
Your Name <egor.lyfar@gmail.com>
2025-09-29 15:56:09 +0800

    chore: add vercel build configuration

---
+++ scripts/vercel-build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+FLUTTER_VERSION="3.29.3"
+FLUTTER_ARCHIVE="flutter_linux_${FLUTTER_VERSION}-stable.tar.xz"
+
+# Download flutter SDK if not cached by Vercel
+if [ ! -d "flutter" ]; then
+  curl -L "https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/${FLUTTER_ARCHIVE}" -o flutter_sdk.tar.xz
+  tar xf flutter_sdk.tar.xz
+fi
+
+export PATH="${PWD}/flutter/bin:${PATH}"
+flutter config --no-analytics
+flutter precache --web
+flutter pub get
+flutter build web --release
